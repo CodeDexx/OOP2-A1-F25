9a8f2a3b613b5a457fb335d9f4bc53ea9586f7af
CodeDexx <ianraharolahy@gmail.com>
2025-09-17 22:52:19 -0400

    Make Person fields final and add toString method
    Changed aName, aBirthDate, and aEmailAddress fields in Person to be final for immutability. Added an overridden toString() method to provide a string representation of Person instances.

--- src/main/java/com/champlain/oop2a1/Person.java
+++ src/main/java/com/champlain/oop2a1/Person.java
@@ -3,9 +3,9 @@ package com.champlain.oop2a1;
 import java.time.LocalDate;
 
 public class Person {
-    private String aName;
-    private LocalDate aBirthDate;
-    private String aEmailAddress;
+    private final String aName;
+    private final LocalDate aBirthDate;
+    private final String aEmailAddress;
     private boolean aHasParkingPass = false;
 
     public Person(String pName, LocalDate pBirthDate, String pEmailAddress) {
@@ -29,4 +29,9 @@ public class Person {
     public boolean isPurchasedParkingPass() {
         return this.aHasParkingPass;
     }
+    @Override
+    public String toString() {
+        return this.aName + " (" + this.aBirthDate + ", " + this.aEmailAddress + ")";
+    }
+
 }
