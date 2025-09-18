a7b8ef6ac7b604cf3e88a499f0cbd61ba66a9d2d
CodeDexx <ianraharolahy@gmail.com>
2025-09-18 14:31:43 -0400

    Add JavaDoc comments to Person class methods
    Added placeholder JavaDoc comments to the Person class and its methods to improve code documentation and maintainability.

--- src/main/java/com/champlain/oop2a1/Person.java
+++ src/main/java/com/champlain/oop2a1/Person.java
@@ -1,3 +1,7 @@
+/**
+ *
+ *
+ */
 package com.champlain.oop2a1;
 
 import java.time.LocalDate;
@@ -8,6 +12,10 @@ public class Person {
     private final String aEmailAddress;
     private boolean aHasParkingPass = false;
 
+    /**
+     *
+     * @return
+     */
     public Person(String pName, LocalDate pBirthDate, String pEmailAddress) {
         if (pName == null || pName.isEmpty()) {
             throw new IllegalArgumentException("The name field is wrong!");
@@ -18,31 +26,53 @@ public class Person {
         if  (pEmailAddress == null || !pEmailAddress.contains("@")) {
             throw new IllegalArgumentException("The Email field is wrong!");
         }
-        
+
         this.aName = pName;
         this.aBirthDate = pBirthDate;
         this.aEmailAddress = pEmailAddress;
-
-
-
     }
+    /**
+     *
+     * @return
+     */
     public boolean purchaseParkingPass() {
         if (aHasParkingPass) return false;
         aHasParkingPass = true;
         return true;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return this.aName;
     }
+    /**
+     *
+     * @return
+     */
     public LocalDate getDOB() {
         return this.aBirthDate;
     }
+    /**
+     *
+     * @return
+     */
     public String getEmailAddress() {
         return this.aEmailAddress;
     }
+    /**
+     *
+     * @return
+     */
     public boolean isPurchasedParkingPass() {
         return this.aHasParkingPass;
     }
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return this.aName + " (" + this.aBirthDate + ", " + this.aEmailAddress + ")";
