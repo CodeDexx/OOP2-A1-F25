313c2d50afcd1f5d3c35ed8b9fd8b4241e88c4fe
CodeDexx <ianraharolahy@gmail.com>
2025-09-18 20:05:45 -0400

    Improve JavaDoc comments in Person class
    Enhanced and clarified JavaDoc comments for all methods and the class itself in Person.java. Added parameter, return, and exception details to improve code documentation and maintainability.

--- src/main/java/com/champlain/oop2a1/Person.java
+++ src/main/java/com/champlain/oop2a1/Person.java
@@ -1,6 +1,6 @@
 /**
- *
- *
+ * Represents a person with a name, date of birth, email address, and if they have a parking pass.
+ * A person can only purchase a parking pass once.
  */
 package com.champlain.oop2a1;
 
@@ -13,8 +13,12 @@ public class Person {
     private boolean aHasParkingPass = false;
 
     /**
+     * Constructs a new Person with given name, date of birth, and email address.
      *
-     * @return
+     * @param pName the person's name
+     * @param pBirthDate the person's date of birth
+     * @param pEmailAddress the person's email address
+     * @throws IllegalArgumentException if any argument is invalid
      */
     public Person(String pName, LocalDate pBirthDate, String pEmailAddress) {
         if (pName == null || pName.isEmpty()) {
@@ -32,8 +36,10 @@ public class Person {
         this.aEmailAddress = pEmailAddress;
     }
     /**
+     * Purchases a parking pass for this person if they don't have one.
+     * The pass cannot be returned.
      *
-     * @return
+     * @return true if pass was successfully purchased, false if already have one.
      */
     public boolean purchaseParkingPass() {
         if (aHasParkingPass) return false;
@@ -42,36 +48,41 @@ public class Person {
     }
 
     /**
+     * Gets the person's name.
      *
-     * @return
+     * @return the name
      */
     public String getName() {
         return this.aName;
     }
     /**
+     * Gets the person's date of birth.
      *
-     * @return
+     * @return the date of birth
      */
     public LocalDate getDOB() {
         return this.aBirthDate;
     }
     /**
+     * Gets the person's email address.
      *
-     * @return
+     * @return the email address
      */
     public String getEmailAddress() {
         return this.aEmailAddress;
     }
     /**
+     * Checks if person has already purchased a parking pass.
      *
-     * @return
+     * @return tru if the person has a pass, false otherwise
      */
     public boolean isPurchasedParkingPass() {
         return this.aHasParkingPass;
     }
     /**
+     * Formats the method to nicely present the text from person.
      *
-     * @return
+     * @return a string with the person's name, date of birth, and email address
      */
     @Override
     public String toString() {
